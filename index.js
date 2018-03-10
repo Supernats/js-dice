565066923144e138470f8573099358d905c4eda8
Nathan Seither <nathanseither@gmail.com>
2018-03-09 18:41:45 -0800

    Actually write the proper index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   Extract: require('./src/extract.js').Extract,
   Roll: require('./src/roll.js').Roll,
-  Sentence: require('./src/roll.js').Roll,
+  Sentence: require('./src/sentence.js').Sentence,
   Value: require('./src/value.js').Value
 };
