71b0cf12d910e1340b48f8ae0daf614fbf47f6ec
Nathan Seither <nathanseither@gmail.com>
2018-03-09 17:05:45 -0800

    Fix imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  Extract: require('../src/extract.js').Extract,
-  Roll: require('../src/roll.js').Roll,
-  Sentence: require('../src/roll.js').Roll,
-  Value: require('../src/value.js').Value
+  Extract: require('./src/extract.js').Extract,
+  Roll: require('./src/roll.js').Roll,
+  Sentence: require('./src/roll.js').Roll,
+  Value: require('./src/value.js').Value
 };
